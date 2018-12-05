a62be89ce075a31688ec0879405bfcf0fac97c28
srydzewski <srydzewski21@amherst.edu>
2018-12-04 21:16:30 -0500

    making create train dict and create test dict

--- create_data.py
+++ create_data.py
@@ -22,8 +22,57 @@ def create_graph():
     pickle.dump((master_dict, desc), pickle_out)
     pickle_out.close()
 
+def create_train_dict():
+    train = open("./data/posts_train.txt", "r")
+    lines = train.readlines()
+    # print(len(lines))
+    master_dict = dict()
+
+    for l in range(1, len(lines)):
+        line = lines[l]
+        nums = line.split(",")
+        key = int(nums[0])
+        value = []
+        for i in range(1, len(nums)):
+            value.append(float(nums[i]))
+
+        if key in master_dict:
+            print("duplicate")
+        else:
+            master_dict[key] = value
+
+    pickle_out = open("./data/posts_train_dict.pkl", 'wb')
+    desc = "a dictionary of the training points with ids as keys and Hour1,Hour2,Hour3,Lat,Lon,Posts as values"
+    pickle.dump((master_dict, desc), pickle_out)
+    pickle_out.close()
+
+def create_test_dict():
+    test = open("./data/posts_test.txt", "r")
+    lines = test.readlines()
+    master_dict = dict()
+
+    for l in range(1, len(lines)):
+        line = lines[l]
+        nums = line.split(",")
+        key = int(nums[0])
+        value = []
+        for i in range(1, len(nums)):
+            value.append(float(nums[i]))
+
+        if key in master_dict:
+            print("duplicate")
+        else:
+            master_dict[key] = value
+
+
+    pickle_out = open("./data/posts_test_dict.pkl", 'wb')
+    desc = "a dictionary of the training points with ids as keys and Hour1,Hour2,Hour3,Posts as values"
+    pickle.dump((master_dict, desc), pickle_out)
+    pickle_out.close()
 
 
 
 if __name__ == '__main__':
     create_graph()
+    create_train_dict()
+    create_test_dict()
