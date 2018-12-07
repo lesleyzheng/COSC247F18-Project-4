3b998b4e5953f62a44c9ac82bef152f3ce28ff36
srydzewski <srydzewski21@amherst.edu>
2018-12-07 18:51:58 -0500

    trying so hard

--- basic_learners.py
+++ basic_learners.py
@@ -1,16 +1,16 @@
 import numpy as np
-import  pickle
+import pickle
 import sklearn
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import zero_one_loss, precision_score, recall_score, confusion_matrix, f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 
-def run_log_reg(X_tr, y_tr):
-    learner = LogisticRegression()
+def run_lin_reg(X_tr, y_tr):
+    learner = LinearRegression()
     learner.fit(X_tr, y_tr)
     train_preds = learner.predict(X_tr)
     train_loss = zero_one_loss(y_tr, train_preds)
@@ -49,3 +49,26 @@ def run_grid_search(X_tr, y_tr):
 
     print("The best parameters found were: ")
     print(gs.best_params_)
+
+
+def get_target_array(target_dict):
+    target_list = list(target_dict.values())
+
+    return np.array(target_list)
+
+
+if __name__ == '__main__':
+    pickle_in = open("./data/target_values.pkl", "rb")
+    (master_lat, master_long, master_loc), desc = pickle.load(pickle_in)
+
+    pickle_in2 = open("./data/master_features.pkl", "rb")
+    master_features, desc2 = pickle.load(pickle_in2)
+
+    master_features = np.array(master_features)
+
+    target_array = get_target_array(master_lat)
+    print(master_features.shape)
+    print(target_array.shape)
+
+    run_lin_reg(master_features, target_array)
+
