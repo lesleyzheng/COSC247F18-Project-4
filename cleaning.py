4a67b83671f0f7edc7e8964e109ef59528d856eb
lesleyzheng <lzheng21@amherst.edu>
2018-12-07 13:30:37 -0500

    cleaning python file initial code

---
+++ cleaning.py
@@ -0,0 +1,107 @@
+'''
+
+6.	Data Cleaning
+a.	Exclude Null Islands
+i.	First see % of total data set  reflect % of test set
+ii.	There could be a certain set of characteristics for null islanders
+b.	Check if all users exist in dataset – check if user id follows a counter - NO
+c.	Check each user has all valid features
+i.	What to do with null island?
+ii.	What to do with 25?
+1.	How should we include it?
+a.	Additional features indicating whether original features have valid inputs
+d.	Outlier detection
+i.	Pca/clustering/general – centrally xxx
+1.	Give me distance from each point; plot and get rid of obvious outliers
+2.	Could just plot
+e.	What to do with users with no connections
+i.	Could give some indication
+
+1.	First predict latitude, then predict longitude, and vice versa
+2.	Predict latitude, then include latitude as a feature, then predict longitude
+3.	Reverse that
+
+
+'''
+
+import numpy as np
+
+
+class cleaning(object):
+
+    def __init__(self):
+
+        self.users = []
+        self.unique_ids = set()
+        self.num_users = 0
+        self.null_islanders = set()
+        self.miss_features = set()
+
+        #hours stuff
+        self.hour1 = set()
+        self.hour2 = set()
+        self.hour3 = set()
+
+        self.invalid_num_posts = set()
+
+    def start(self):
+
+        f = open("./data/posts_train.txt", "r")
+
+        first_line = True
+        for line in f:
+
+            if first_line:
+                first_line = False
+            else:
+                user_info = line.split(",")
+                self.unique_ids.add(user_info[0])
+                self.all_features(user_info)
+                self.null_islander(user_info)
+                self.invalid_hours(user_info)
+                self.users.apppend(user_info)
+
+        print(f"There are {len(self.unique_ids)} unique IDs.")
+        print(f"There are {len(self.users)} number of users.")
+        print(f"There are {len(self.null_islanders)} number of null islands.")
+        print(f"There are {len(self.hour1)} number of invalid hour1s, {len(self.hour2)} number of invalid hour2s "
+              f"and {len(self.hour3)} number of invalid hour3s.")
+
+        print(f"Intersections")
+        print(len(self.null_islanders.intersection(self.hour1)))
+        print(len(self.hour1.intersection(self.hour2)))
+        print(len(self.hour2.intersection(self.hour3)))
+        print(len(self.hour1.intersection(self.hour3)))
+        print(len(self.hour1.intersection(self.hour2).intersection(self.hour3)))
+        print(len(self.hour1.intersection(self.hour2).intersection(self.hour3).intersection(self.null_islanders)))
+
+    def all_features(self, user_array):
+
+        if len(user_array) != 7:
+            print(f"Missing features for {user_array[0]}")
+            self.miss_features.add(user_array[0])
+
+    def null_islander(self, user_array):
+
+        # Id,Hour1,Hour2,Hour3,Lat,Lon,Posts
+        if user_array[4] == 0.0 and user_array[5] == 0.0:
+            self.null_islanders.add(user_array[0])
+
+    def invalid_hours(self, user_array):
+
+        if user_array[1] == 25:
+            self.hour1.add(user_array[0])
+        if user_array[2] == 25:
+            self.hour2.add(user_array[0])
+        if user_array[3] == 25:
+            self.hour3.add(user_array[0])
+
+        for i in range(1,4):
+            if user_array[i] not in np.arange(1, 26):
+                print(f"{user_array[0]} has an hour out of range")
+
+
+if __name__ == "__main__":
+
+    myClean = cleaning()
+    myClean.start()
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("hello")
+f = open("data/posts_train.txt", "r")
+
+text = f.read()
+
+print(text)
